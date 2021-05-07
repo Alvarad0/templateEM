1c0fe586e89b49cd06bb742350dea99d3849e558
Alvarado <alvarado.programador@gmail.com>
2021-05-07 16:32:15 -0500

    Seccion Principal, ultimas noticias, videos, redes sociales

---
+++ js/main.js
@@ -0,0 +1,57 @@
+function init() {
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    popoverTriggerList.map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl))
+
+    $("#btnSearch").click(() => {
+        $("#btnSearch").hide()
+        $("#boxSearch").css("display", "flex")
+        $("#boxSearch input").focus()
+    })
+    $("#boxSearch button").click(() => {
+        $("#btnSearch").show()
+        $("#boxSearch").css("display", "none")
+        $("#boxSearch input").val("")
+    })
+
+    $("#mainCarousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        dots: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        animateOut: 'fadeOut',
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+    $("#carouselPeople").owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        animateOut: 'fadeOut',
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    })
+}
+
+init()
